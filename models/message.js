b7441de55163b68423dade19ecfe30efd3c5fd57
Snack-Time <wmcnally1226@gmail.com>
2024-05-07 00:52:47 -0400

    create schema models

---
+++ models/message.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+const Schema = mongoose.Schema;
+
+const MsgSchema = new Schema({
+    content: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
+    original_poster: { type: Schema.Types.ObjectId, ref: "User", required: true },
+})
+
+MsgSchema.virtual("timestamp_formatted").get(function() {
+    return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)
+})
+
+module.exports = mongoose.model("Message", MsgSchema);
