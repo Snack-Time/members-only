c2116902230996cd63f986a3fbc5b12f330370fa
Snack-Time <wmcnally1226@gmail.com>
2024-05-09 02:24:45 -0400

    work on validating form

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -8,9 +8,38 @@ exports.user_create_get = asyncHandler(async (req, res, next) => {
     res.render("sign-up", { title: 'Sign Up' })
 });
 
-exports.user_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: User Create Post")
-});
+exports.user_create_post = [
+    body('username', 'Username must be at least 3 characters!')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+    body('username', 'Username already in use!')
+    .custom(async value => {
+        const user = await User.find({ name: req.body.username })
+    }),
+    body('password', 'Password must be at least 5 characters!')
+    .trim()
+    .isLength({ min: 5 })
+    .escape(),
+    body('confirmpassword', 'Password must match!')
+    .custom((value, { req }) => {
+        return value === req.body.password;
+    }),
+    
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        console.log(errors)
+        if (!errors.isEmpty()) {
+            res.render('sign-up', {
+                title: 'Sign Up',
+                errors: errors.array()
+            })
+            return
+        }
+        else {
+            res.redirect('/')
+        }
+})];
 
 exports.message_create_get = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED: Message Create Get")
